fcfffb4bbea1109ad874d701f2e86dd7e695068f
Hanadi Weldali <hanadiweldali95@gmail.com>
2026-02-10 00:45:27 +0200

    add search bar to navbar

--- src/components/navbar/MainNavbar.jsx
+++ src/components/navbar/MainNavbar.jsx
@@ -8,7 +8,7 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Container, Link ,Stack ,Select , MenuItem } from '@mui/material';
 import { Link as Routerlink} from "react-router-dom";
-
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 export default function MainNavbar() {
   return (
@@ -18,9 +18,8 @@ export default function MainNavbar() {
           <Toolbar>
             <Box sx={{ display: "flex", justifyContent:"space-between", width:"100%"}}>
             <Box sx={{ display: "flex", gap: 8 , color:"black"}}>
-              <Typography variant="h6" component="div">
-                KAshop
-              </Typography>
+              <Typography variant="h6" component="div">KAshop</Typography>
+          
         
             <Stack direction="row" spacing={3} alignItems="center">
                   <Link color="inherit" component={Routerlink} to="/" underline='none'>HOME</Link>
@@ -29,17 +28,28 @@ export default function MainNavbar() {
             <MenuItem value="About" component={Routerlink} to="/about">About</MenuItem>
                  <MenuItem value="Cart" component={Routerlink} to="/cart">Cart</MenuItem>
                   <MenuItem value="Checkout" component={Routerlink} to="/checkout">Checkout</MenuItem>
-                   <MenuItem value="Product" component={Routerlink} to="/product">Product</MenuItem>
+                   
           </Select>
           <Link color="inherit" component={Routerlink} to="/contact" underline='none'>CONTACT</Link>
+          <Link color="inherit" component={Routerlink} to="/product" underline='none'>PRODUCT</Link>
             </Stack>
             </Box>
-
+           <Box sx={{display:"flex",gap:2}}>
+            <FavoriteBorderIcon sx={{color: "black",fontSize: 20,width: 40,height: 40,border: "1px solid black",borderRadius: "50%",   padding: "8px",display: "flex",alignItems: "center",justifyContent: "center"}}/>
             <Box sx={{display:"flex" , flexDirection:"column" ,color:"black"}}>
                 <Typography variant="body2" fontWeight="bold">Welcome</Typography>
             <Box>
                  <Link color="inherit" component={Routerlink} to="/login" underline='none'>Login / </Link>
                   <Link color="inherit" component={Routerlink} to="/register" underline='none'>Register</Link>
+                  
+            </Box>
+            </Box>
+            <Box sx={{display:"flex",gap:2}}>
+           <Box sx={{width: 40,height: 40,border: "1px solid black",borderRadius: "50%",   padding: "8px"}}></Box>
+           <Box sx={{display:"flex",flexDirection:"column",color:"black"}}>
+             <Typography variant="body2" fontWeight="bold">Cart</Typography>
+             <Typography variant="body2" fontWeight="bold">$1,689.00</Typography>
+           </Box>
             </Box>
             </Box>
             </Box>
--- src/components/navbar/SearchBar.jsx
+++ src/components/navbar/SearchBar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
-
+import { Box, TextField, InputAdornment, MenuItem } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 export default function SearchBar() {
-  return (
-    <div>SearchBar</div>
-  )
+ return (
+    <Box sx={{background: "#19b21a", padding: "20px",borderRadius: "12px",display: "flex",alignItems: "center",justifyContent: "space-between"}}>
+      
+      <Box sx={{display: "flex",alignItems: "center",background: "#f1f1f1",borderRadius: "30px",px: 2,width: "500px"}}>
+        
+        <TextField select variant="standard" defaultValue="all" InputProps={{ disableUnderline: true }} sx={{ width: 150 }}>
+          <MenuItem value="all">All Categories</MenuItem>
+        
+        </TextField>
+
+       
+        <TextField placeholder="Search anything..." variant="standard" fullWidth InputProps={{disableUnderline: true,endAdornment: (
+              <InputAdornment position="end">
+                <SearchIcon sx={{ color: "black", cursor: "pointer" }} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      
+      <Box sx={{ color: "white", display: "flex", gap: 4, fontSize: 14 }}>
+        <span>FREE SHIPPING OVER $199</span>
+        <span>30 DAYS MONEY BACK</span>
+        <span>100% SECURE PAYMENT</span>
+      </Box>
+    </Box>
+  );
 }
